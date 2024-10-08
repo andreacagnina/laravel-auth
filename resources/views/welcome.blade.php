d35e1f49be12aa7843ff330008cc02530eb1d339
andreacagnina <amirkal@libero.it>
2024-10-08 15:44:52 +0200

    [feat]

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,11 @@
                 <div class="content">
                     <h2>Contenuto pubblico</h2>
                 </div>
+                @guest
+                    Non sei loggato
+                @else
+                    Sei loggato
+                @endguest
             </div>
         </div>
     </div>
